1f578956041fc4ef741bc4a1308f7fb8ad1269e6
da.chengd <da.chengd@alibaba-inc.com>
2017-06-25 12:12:18 +0800

    -----------------

---
+++ src/test/java/com/topalps/java/Time.java
@@ -0,0 +1,17 @@
+package com.topalps.java;
+
+import org.junit.Test;
+
+/**
+ * Created by 休清 on 2017/6/25.
+ *
+ * @author 休清
+ * @date 2017/06/25
+ */
+public class Time {
+
+    @Test
+    public void f() {
+
+    }
+}
